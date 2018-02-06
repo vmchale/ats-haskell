235e37e1503848200234faf18ab6c17980f88d6d
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-02-06 14:20:08 -0600

    add pragmas

--- hs/{{ Project }}.hs
+++ hs/{{ Project }}.hs
@@ -1,7 +1,4 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
-{-# LANGUAGE DeriveGeneric            #-}
-{-# LANGUAGE DeriveAnyClass           #-}
-{-# LANGUAGE IncoherentInstnances     #-}
 
 module {{ Project }} where
 
